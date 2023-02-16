8121700dc6b15deeb9aee2ff8da6bca3a75350ea
Djonathan <djonathandavila.dev@gmail.com>
2023-02-16 10:18:55 -0300

    First commit

---
+++ app/build/tmp/kapt3/stubs/debug/com/example/android/dessertclicker/MainActivityKt.java
@@ -0,0 +1,15 @@
+package com.example.android.dessertclicker;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 7, 1}, k = 2, d1 = {"\u0000\u0012\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u0003\n\u0002\u0010\b\n\u0002\b\u0002\"\u000e\u0010\u0000\u001a\u00020\u0001X\u0086T\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\u0002\u001a\u00020\u0001X\u0086T\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\u0003\u001a\u00020\u0001X\u0086T\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\u0004\u001a\u00020\u0005X\u0082\u000e\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\u0006\u001a\u00020\u0005X\u0082\u000e\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u0007"}, d2 = {"KEY_DESSERT_SOLD", "", "KEY_REVENUE", "TAG", "dessertsSold", "", "revenue", "app_debug"})
+public final class MainActivityKt {
+    @org.jetbrains.annotations.NotNull()
+    public static final java.lang.String TAG = "MainActivity";
+    @org.jetbrains.annotations.NotNull()
+    public static final java.lang.String KEY_REVENUE = "revenue_key";
+    @org.jetbrains.annotations.NotNull()
+    public static final java.lang.String KEY_DESSERT_SOLD = "dessert_sold_key";
+    private static int revenue = 0;
+    private static int dessertsSold = 0;
+}
